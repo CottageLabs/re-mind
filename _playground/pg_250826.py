898e14364b0247338f7977076e96a12f2a2ea974
seabird64 <seabird64@email.not.exit.io>
2025-10-03 09:42:44 +0100

    Update playground script imports to use librarian module

--- _playground/pg_250826.py
+++ _playground/pg_250826.py
@@ -9,13 +9,13 @@ from rich.markdown import Markdown
 from rich.rule import Rule
 
 from re_mind import components, pipelines
-from re_mind.db.qdrant.qdrant import get_client
+from librarian.db.qdrant import get_client
 from re_mind.language_models import create_llm_huggingface
 from re_mind.llm_tasks import extract_queries_from_input, retrieve_and_deduplicate_docs
 from re_mind.pipelines import build_rag_app
 from re_mind.rag.rag_chat import RagChat
 from re_mind.rankers import rerankers
-from re_mind.text_processing import save_pdf_to_vectorstore
+from librarian.text_processing import save_pdf_to_vectorstore
 from re_mind.utils import re_mind_utils as llm_utils, iter_utils
 from re_mind.utils.raq_utils import print_result
 
