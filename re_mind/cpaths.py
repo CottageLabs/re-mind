52380f6e3b95ae3e5a6eb3808a02234c29db3fda
philip <philip.k@cottagelabs.com>
2025-10-30 12:29:48 +0000

    Add configuration file path documentation in cpaths.py

--- re_mind/cpaths.py
+++ re_mind/cpaths.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 PROJ_HOME = Path.home().joinpath('.local/opt/re-mind')
+
 QDRANT_DATA_PATH = PROJ_HOME / 'qdrant'
 
 CONFIG_PATH = PROJ_HOME / 'config.json'
+""" Configuration file path for the Re-Mind Command line application. """
