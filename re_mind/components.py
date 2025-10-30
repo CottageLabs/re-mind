81276fed09faf2eb7e39be39121ca79c9e58a586
philip <philip.k@cottagelabs.com>
2025-10-30 12:29:57 +0000

    Refactor get_client and get_vector_store functions for improved path handling

--- re_mind/components.py
+++ re_mind/components.py
@@ -1,6 +1,15 @@
 from librarian.components import (  # noqa
-    get_vector_store,
-    get_client,
     get_embedding,
 )
 from llmchat.language_models import get_llm  # noqa
+
+
+def get_client(**kwargs):
+    from librarian.components import get_client as get_client_impl
+    from re_mind import cpaths
+    return get_client_impl(path=cpaths.QDRANT_DATA_PATH, **kwargs)
+
+
+def get_vector_store(**kwargs):
+    from librarian.components import get_vector_store as get_vector_store_impl
+    return get_vector_store_impl(client=get_client(), **kwargs)
