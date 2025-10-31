39eb2babbf1a0478bddee5478894b48275c016f7
philip <philip.k@cottagelabs.com>
2025-10-31 09:51:12 +0000

    Add 'status' command to display library status and collections in Qdrant

--- re_mind/envvars.py
+++ re_mind/envvars.py
@@ -6,9 +6,8 @@ from .cpaths import PROJ_HOME
 
 def get_qdrant_data_path() -> Path:
     """Get Qdrant data path from environment variable or default."""
-    default_path = PROJ_HOME / 'qdrant'
     env_value = os.getenv('QDRANT_DATA_PATH')
 
     if env_value:
         return Path(env_value)
-    return default_path
+    return PROJ_HOME / 'qdrant'
