6407ba1e4aab786f1ee1aa41a5eee33f3e8ac9e6
philip <philip.k@cottagelabs.com>
2025-11-03 13:53:29 +0000

    refactor: update default collection name for consistency across constants

--- re_mind/constants.py
+++ re_mind/constants.py
@@ -2,4 +2,4 @@ LLM_MODEL_NAME = 'google/gemma-3-1b-it'
 
 DEFAULT_EMBEDDING = "sentence-transformers/all-MiniLM-L6-v2"
 DEFAULT_EMBEDDING_DIM = 384
-DEFAULT_COLLECTION_NAME = 're_mind_default_collection'
+DEFAULT_COLLECTION_NAME = '__default_collection__'
